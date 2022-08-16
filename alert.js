d307bfd0e66b5d6ba40c22249095bc39d2d780ee
Kwanwoo Baek <kwanwoob@usc.edu>
2022-08-16 02:13:23 -0600

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,7 +1,7 @@
 document.querySelector("#share").addEventListener("click", function() {
-    alert("Due to this website's original host provider being no longer in service, the database features are no longer available.")
+    alert("Due to this website's original host provider being no longer in service, features with the database are no longer accesible.")
 });
 
 document.querySelector("#search").addEventListener("click", function() {
-    alert("Due to this website's original host provider being no longer in service, the database features are no longer available.")
+    alert("Due to this website's original host provider being no longer in service, features with the database are no longer accesible.")
 });
